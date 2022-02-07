eb200e24d8bf3529822f3999d7ce77b1c8bdea99
jibola <pro.ajibolaojo@gmail.com>
2022-02-07 12:53:22 +0100

    cors allowing all

--- hms/settings.py
+++ hms/settings.py
@@ -26,11 +26,13 @@ INSTALLED_APPS = [
     'core.apps.CoreConfig',
     'rest_framework',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -39,6 +41,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'hms.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
